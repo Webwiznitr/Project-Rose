a6142f7cc5e49d1975e2cb7b28dd835d0865d7c7
Ankit Kiran <95562947+Aankirz@users.noreply.github.com>
2023-11-03 07:07:38 +0000

    okupd

--- sac-website/next.config.js
+++ sac-website/next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
   images: {
     domains: ["res.cloudinary.com", "ik.imagekit.io", "images.unsplash.com"],
+    unoptimized: true,
   },
+  
+  output: 'export',
+  
 };
 
 module.exports = nextConfig;
